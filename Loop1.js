de43a124e22d69a74ca50ea8f2dbecb2443e71b6
Amirsohail335 <amirsohail335@gmail.com>
2025-09-12 22:13:39 +0530

    array

--- Loop1.js
+++ Loop1.js
@@ -1,3 +1,9 @@
-for (let i = 1; i < 11; i++) {
-  console.log(i + ": hi");
+// for (let i = 1; i < 11; i++) {
+//   console.log(i + ": hi");
+// }
+
+let arr = [1, 3, 4, 5, 5, 4, 64, 3, 42, 2, 3];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
