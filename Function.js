87a45c4fc907cbabeada3d07cad1715e9e96dac7
Amirsohail335 <amirsohail335@gmail.com>
2025-09-12 22:00:32 +0530

    committed

--- Function.js
+++ Function.js
@@ -3,4 +3,31 @@ function sum(a, b) {
   console.log("Total sum :" + total);
 }
 
-sum(4,5)
+// sum(4,5)
+
+function isEligibleToVote(age) {
+  if (age < 0) {
+    console.log("Invalid age");
+  } else if (age < 18) {
+    console.log("Not Eligible to Vote");
+  } else {
+    console.log("Elgible to Vote");
+  }
+}
+
+// isEligibleToVote(12);
+// isEligibleToVote(-1);
+// isEligibleToVote(19);
+
+//To check a number is even or odd
+
+function OddEven(num) {
+  if (num % 2 == 0) {
+    console.log(num + " is even");
+  } else {
+    console.log(num + " is Odd");
+  }
+}
+
+OddEven(3)
+OddEven(6)
