7a2d43860adea8ab260d15c7e7273bb57b3edf19
Amirsohail335 <amirsohail335@gmail.com>
2025-09-13 01:18:09 +0530

    search element

--- Loop1.js
+++ Loop1.js
@@ -4,6 +4,25 @@
 
 let arr = [1, 3, 4, 5, 5, 4, 64, 3, 42, 2, 3];
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+function searchElement(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == num) {
+      return i;
+    }
+  }
+  return -1;
 }
+
+// searchElement(arr, 3);
+const result = searchElement(arr, 11);
+console.log(result);
