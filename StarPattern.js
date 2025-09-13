a591ceea665a75ed68d42658beed407a9719f266
Amirsohail335 <amirsohail335@gmail.com>
2025-09-13 23:54:47 +0530

    number pattern

--- StarPattern.js
+++ StarPattern.js
@@ -1,4 +1,4 @@
-let n = 4;
+let n = 5;
 
 // for(let i=0; i<n;i++){
 //     let row = ""
@@ -17,9 +17,17 @@ let n = 4;
 // }
 
 for (let i = 0; i < n; i++) {
-  let row ="";
+  let row = "";
   for (let j = 1; j <= i + 1; j++) {
     row = row + j;
+  }
+  console.log(row);
+}
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (j = 0; j < i + 1; j++) {
+    row = row + (i + 1);
   }
   console.log(row);
 }
