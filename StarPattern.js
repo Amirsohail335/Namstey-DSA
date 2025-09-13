a7a79798274a83534d9c23497bc146a3a398b8f3
Amirsohail335 <amirsohail335@gmail.com>
2025-09-14 01:00:18 +0530

    toggle

--- StarPattern.js
+++ StarPattern.js
@@ -32,10 +32,36 @@ let n = 5;
 //   console.log(row);
 // }
 
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (j = 0; j < n - i; j++) {
+//     row = row + (j + 1);
+//   }
+//   console.log(row);
+// }
+
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (j = 0; j < n - (i + 1); j++) {
+//     row = row + " ";
+//   }
+//   for (k = 0; k < i + 1; k++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
 for (let i = 0; i < n; i++) {
   let row = "";
-  for (j = 0; j < n - i; j++) {
-    row = row + (j + 1);
+  let toggle = 1;
+  for (let j = 0; j < i + 1; j++) {
+    row = row + toggle;
+
+    if (toggle == 1) {
+      toggle = 0;
+    } else {
+      toggle = 1;
+    }
   }
   console.log(row);
 }
