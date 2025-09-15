336ce236455cda7e2162443cf6deb50c8403bda5
Amirsohail335 <amirsohail335@gmail.com>
2025-09-15 23:25:52 +0530

    comitted

--- StarPattern.js
+++ StarPattern.js
@@ -16,13 +16,13 @@ let n = 5;
 //   console.log(row);
 // }
 
-// for (let i = 0; i < n; i++) {
-//   let row = "";
-//   for (let j = 1; j <= i + 1; j++) {
-//     row = row + j;
-//   }
-//   console.log(row);
-// }
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 1; j <= i + 1; j++) {
+    row = row + j;
+  }
+  console.log(row);
+}
 
 for (let i = 0; i < n; i++) {
   let row = "";
