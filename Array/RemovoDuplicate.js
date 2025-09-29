0c90664e82a0d7aeb49dcdcefcddcb6f209abb1a
Amirsohail335 <amirsohail335@gmail.com>
2025-09-29 17:51:33 +0530

    comitted

--- Array/RemovoDuplicate.js
+++ Array/RemovoDuplicate.js
@@ -11,6 +11,6 @@ function RemoveDuplicate(nums) {
   return x + 1;
 }
 
-let arr = [1, 1, 2, 4, 4, 5, 6, 7, 7, 8];
+let arr = [1, 1, 2, 4, 4, 5, 6,  7, 8];
 let res = RemoveDuplicate(arr);
 console.log(res);
