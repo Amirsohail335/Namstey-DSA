9c738d03865ec3919bd293aa3a8ea5ab841ac85d
Amirsohail335 <amirsohail335@gmail.com>
2025-10-04 09:01:35 +0530

    Remove Duplicate

---
+++ Array/RemoveDuplicate.js
@@ -0,0 +1,16 @@
+function RemoveDuplicate(nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x = x + 1;
+      nums[x] = nums[i];
+    }
+  }
+  let uniqueElements = nums.slice(0, x + 1);
+  console.log(uniqueElements,'Uniques Elements');
+  return x + 1;
+}
+
+let arr = [1, 1, 2, 4, 4, 5, 6,  7, 8];
+let res = RemoveDuplicate(arr);
+console.log(res);
