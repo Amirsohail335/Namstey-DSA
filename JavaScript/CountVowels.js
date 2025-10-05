3c32e94a22e458e690d2033d8fb44b7185143d30
Amirsohail335 <amirsohail335@gmail.com>
2025-10-05 12:16:11 +0530

    both vowels and constant

--- JavaScript/CountVowels.js
+++ JavaScript/CountVowels.js
@@ -1,16 +1,43 @@
 function CountVowels(str) {
   let vowels = "aeiouAEIOU";
   let count = 0;
-  let foundVowels = []
+  let foundVowels = [];
 
   for (let char of str) {
     if (vowels.includes(char)) {
       count++;
 
-      foundVowels.push(char)
+      foundVowels.push(char);
     }
   }
-  return { vowels: count , foundVowels:foundVowels};
+  return { vowels: count, foundVowels: foundVowels };
 }
 
-console.log(CountVowels("Amir Sohail"))
+function CountVowelsAndConstant(str) {
+  let vowels = "aeiouAEIOU";
+  let vowelsCount = 0;
+  let constantCount = 0;
+  let foundVowels = [];
+  let foundConstant = [];
+
+  for (let char of str) {
+    if (/[a-zA-Z]/.test(char)) {
+      if (vowels.includes(char)) {
+        vowelsCount++;
+        foundVowels.push(char);
+      } else {
+        constantCount++;
+        foundConstant.push(char);
+      }
+    }
+  }
+  return {
+    vowelsCount: vowelsCount,
+    constantCount: constantCount,
+    foundConstant: foundConstant,
+    foundVowels: foundVowels,
+  };
+}
+
+// console.log(CountVowels("Amir Sohail"));
+console.log(CountVowelsAndConstant("Amir Sohail"));
