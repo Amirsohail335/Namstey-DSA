541e81e142d89465d42e401469be2375183a2708
Amirsohail335 <amirsohail335@gmail.com>
2025-10-05 12:04:40 +0530

    show vowels

--- JavaScript/CountVowels.js
+++ JavaScript/CountVowels.js
@@ -1,13 +1,16 @@
 function CountVowels(str) {
   let vowels = "aeiouAEIOU";
   let count = 0;
+  let foundVowels = []
 
   for (let char of str) {
     if (vowels.includes(char)) {
       count++;
+
+      foundVowels.push(char)
     }
   }
-  return { vowels: count };
+  return { vowels: count , foundVowels:foundVowels};
 }
 
 console.log(CountVowels("Amir Sohail"))
