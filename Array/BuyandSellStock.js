867c314b7154a257501320fb76bcda6006155cbe
Amirsohail335 <amirsohail335@gmail.com>
2025-12-06 20:30:42 +0530

    Best time to buy stock

---
+++ Array/BuyandSellStock.js
@@ -0,0 +1,20 @@
+function MaxProfit(prices) {
+  let min = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - min > maxProfit) {
+      maxProfit = prices[i] - min;
+    }
+
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+  }
+  return maxProfit;
+}
+
+let p = [2, 1, 4, 9, 6, 5];
+
+let res = MaxProfit(p);
+console.log(res);
